af2c421044af6d6c7e0d883c3b92f8afb6fa291a
Vagner Melo <vagnerbrmelo@gmail.com>
2025-07-10 14:46:02 -0300

    ex08

---
+++ Piscine_Reloaded/ex08/ft_is_negative.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vade-mel <vade-mel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/10 14:11:51 by vade-mel          #+#    #+#             */
+/*   Updated: 2025/07/10 14:14:49 by vade-mel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+		ft_putchar('N');
+	else
+		ft_putchar('P');
+}
+
+/*int	main(void)
+{
+	ft_is_negative(-10);
+	return (0);
+}
+*/
