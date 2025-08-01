36ea70bc4c89a5c4ebd1472f153f8144600e8802
vade-mel <vagnerbrmelo@gmail.com>
2025-08-01 20:58:11 -0300

    updating

---
+++ Libft/ft_strnstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vade-mel <vade-mel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 20:54:48 by vade-mel          #+#    #+#             */
+/*   Updated: 2025/08/01 20:57:40 by vade-mel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (little[0] == '\0')
+		return ((char *)big);
+	while (big[i] != '\0')
+	{
+		j = 0;
+		while (big[i + j] == little[j] && (i + j) < len)
+		{
+			if (big[i + j] == '\0' && little[j] == '\0')
+				return ((char *)&big[i]);
+			j++;
+		}
+		if (little[j] == '\0')
+			return ((char *)big + i);
+		i++;
+	}
+	return (0);
+}
