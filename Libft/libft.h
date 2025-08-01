ae0f0af64154099ea1ae63019d1af07b696f759d
vade-mel <vagnerbrmelo@gmail.com>
2025-08-01 20:07:28 -0300

    add more functions in libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vade-mel <vade-mel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 20:20:35 by vade-mel          #+#    #+#             */
-/*   Updated: 2025/07/21 22:29:18 by vade-mel         ###   ########.fr       */
+/*   Updated: 2025/08/01 20:06:50 by vade-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,19 @@ int			ft_isascii(int c);
 int			ft_atoi(char *str);
 int			ft_isdigit(char c);
 int			ft_isprint(char c);
-size_t		ft_strlen(char *s);
 int			ft_strncmp(char *s1, char *s2, unsigned int n);
+int			ft_toupper(int c);
+int			ft_lower(int c);
+
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
+
+char		*ft_strchr(const char *s, int c);
+
+size_t		ft_strlen(const char *s);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcat(char *dest, char *src, size_t size);
+
 
 #endif
